3869f8f57573e6688d526c74da36211e44df8a47
yukta9499 <yuktakale3105@gmail.com>
2026-09-21 14:49:43 +0600

    loop

---
+++ prob2.py
@@ -0,0 +1,9 @@
+n = 1234
+count = 0
+for i in range (4):
+    n = n//10
+    count +=1
+print(count)
+        
+
+    
---
+++ starpattern.py
@@ -0,0 +1,8 @@
+for i in range(1,5):
+    for j in range (1,(4-i)+1):
+        print(" ",end=" ")
+    for k in range(1,(i*2-1)+1):
+        print("*",end="")
+    print()
+
+
